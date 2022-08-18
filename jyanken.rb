22b3f64e27d1fdea3c2a159c0e2923492b173f34
yamamoto <b11u0155@gmail.com>
2022-08-18 09:23:48 +0000

    あっち向いてホイ

---
+++ jyanken.rb
@@ -0,0 +1,59 @@
+def janken
+puts "[0]:(グー)[1]:(チョキ)[2]:(パー)[3]:(戦わない)"
+
+player_hand = gets.to_i
+program_hand = rand(3)
+jankens = ["グー","チョキ","パー"]
+if player_hand == 3
+  
+  puts "戦わないを選択されたので、ゲームを終了します"
+elsif player_hand == program_hand
+  puts "あなたの手:#{jankens[player_hand]},プログラムが選んだ手:#{jankens[program_hand]}"
+  puts" あいこで"
+  return true
+elsif (player_hand == 0 && program_hand == 1) ||
+      (player_hand == 1 && program_hand == 2) ||
+      (player_hand == 2 && program_hand == 0)
+      puts "あなたの手:#{jankens[player_hand]},プログラムが選んだ手:#{jankens[program_hand]}"
+      puts "あなたの勝ちです"
+      @janken_judge = "win"
+      achimuite
+      
+else 
+  puts "あなたの手:#{jankens[player_hand]},プログラムが選んだ手:#{jankens[program_hand]}"
+  puts "あなたの負けです"
+  @janken_judge = "lose"
+  achimuite
+  
+end
+end
+
+
+def achimuite
+  puts "あっち向いて・・・"
+  puts "[0]:(上)[1]:(下)[2]:(右)[3]:(左)"
+  player_face = gets.to_i
+  program_face = rand(4)
+  achimuites = ["上","下","右","左"]
+  if (player_face == program_face) && (@janken_judge == "win")
+    puts "あなたの指差した方向:#{achimuites[player_face]},プログラムが向いた方向:#{achimuites[program_face]}"
+    puts "あなたの勝ちです"
+  elsif (player_face == program_face) && (@janken_judge == "lose")
+    puts "プログラムが指差した方向:#{achimuites[program_face]},あなたが向いた方向:#{achimuites[player_face]}"
+    puts "あなたの負けです"
+  elsif (player_face != program_face) && (@janken_judge == "win")
+    puts "あなたの指差した方向:#{achimuites[player_face]},プログラムが向いた方向:#{achimuites[program_face]}"
+    puts "コンピュータもなかなかやりますね！"
+    puts "もう一度、じゃんけんから"
+      next_game = true
+  else 
+    puts "プログラムが指差した方向:#{achimuites[program_face]},あなたが向いた方向:#{achimuites[player_face]}"
+    puts "あなたもなかなかやりますね！"
+    puts "もう一度、じゃんけんから"
+
+    end
+  end
+  next_game = true
+while next_game
+  next_game = janken
+end
